30c0e1a84e9e950fdd3ab2b7cacedbc2791ac350
David Keyes <dgkeyes@gmail.com>
2019-05-28 11:53:51 -0700

    Add OR CA communities function

--- R/dk_functions.R
+++ R/dk_functions.R
@@ -17,3 +17,48 @@ dk_get_tfff_basemap <- function() {
 }
 
 
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dk_theme <- function() {
+     library(ggplot2)
+
+     theme_minimal() +
+          theme(axis.title = element_blank(),
+                legend.position = "none",
+                text = element_text(family = "Karla",
+                                    face = "bold"))
+}
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dk_get_or_ca_places <- function() {
+        library(tigris)
+        library(tidyverse)
+        library(sf)
+        library(janitor)
+
+        options(tigris_class = "sf")
+
+        or_communities <- places(state = "OR",
+                                 cb = TRUE) %>%
+                st_centroid()
+
+        ca_communities <- places(state = "CA",
+                                 cb = TRUE) %>%
+                st_centroid()
+
+        or_ca_communities <- rbind(or_communities,
+                                   ca_communities) %>%
+                clean_names() %>%
+                select(name)
+
+}
