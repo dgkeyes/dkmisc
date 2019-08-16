c8160816ce73fe87e259c47f55d8e57fb696ac8a
David Keyes <dgkeyes@gmail.com>
2019-08-15 21:09:11 -0700

    Updates

--- R/dk_functions.R
+++ R/dk_functions.R
@@ -7,7 +7,7 @@
 dk_get_tfff_basemap <- function() {
 
         tfff_basemap <- tigris::counties(cb = T, class="sf") %>%
-                clean_names() %>%
+                janitor::clean_names() %>%
                 filter(statefp == "41" | statefp == "06") %>%
                 filter(statefp == "41" | name == "Siskiyou")
 }
@@ -20,7 +20,6 @@ dk_get_tfff_basemap <- function() {
 #'
 #' @examples
 dk_theme <- function() {
-        library(ggplot2)
 
         theme_minimal() +
                 theme(axis.title = element_blank(),
@@ -30,7 +29,8 @@ dk_theme <- function() {
 }
 
 
-#' Title DK Get OR CA Places
+
+#' Title
 #'
 #' @return
 #' @export
@@ -42,15 +42,15 @@ dk_get_or_ca_places <- function() {
 
         or_communities <- tigris::places(state = "OR",
                                  cb = TRUE) %>%
-                st_centroid()
+                sf::st_centroid()
 
         ca_communities <- tigris::places(state = "CA",
                                  cb = TRUE) %>%
-                st_centroid()
+                sf::st_centroid()
 
         or_ca_communities <- rbind(or_communities,
                                    ca_communities) %>%
-                clean_names() %>%
-                select(name)
+                janitor::clean_names() %>%
+                dplyr::select(name)
 
 }
